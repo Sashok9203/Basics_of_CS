048ae98e68c70c4fd2cce391a266f5f8a5f188f6
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-03-22 00:03:00 +0200

    Добавьте файлы проекта.

---
+++ Basics_of_C#/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Basics_of_C_
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            CircleArea();
+
+        }
+
+        static void CircleArea()
+        {
+            int radius;
+            do
+            {
+                Console.Clear();
+                Console.Write(" Enter circle radius : ");
+            } while (!int.TryParse(Console.ReadLine(), out radius));
+            Console.Write($" Area of circle with radius {radius}  =  {Math.Round(Math.PI * Math.Pow(radius,2),2)}");
+            Console.ReadKey();
+        }
+        static void CircleArea()
+        {
+            int radius;
+            do
+            {
+                Console.Clear();
+                Console.Write(" Enter circle radius : ");
+            } while (!int.TryParse(Console.ReadLine(), out radius));
+            Console.Write($" Area of circle with radius {radius}  =  {Math.Round(Math.PI * Math.Pow(radius, 2), 2)}");
+            Console.ReadKey();
+        }
+
+    }
+}
