f41618817e5c247624f8366d46c1162f32556eec
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-03-22 01:07:24 +0200

    Task 1 Task 2 Task3 Task4

--- Basics_of_C#/Program.cs
+++ Basics_of_C#/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Basics_of_C_
+﻿using System.Diagnostics.Metrics;
+
+namespace Basics_of_C_
 {
     internal class Program
     {
@@ -6,31 +8,85 @@
 
         static void Main(string[] args)
         {
-            CircleArea();
-
+            //circleArea();
+            //guessNumber();
+            //drawRectangle();
+            harvestingHay();
         }
 
-        static void CircleArea()
+        static void circleArea()
         {
-            int radius;
-            do
-            {
-                Console.Clear();
-                Console.Write(" Enter circle radius : ");
-            } while (!int.TryParse(Console.ReadLine(), out radius));
+            int radius = getValue(" Enter circle radius : ");
             Console.Write($" Area of circle with radius {radius}  =  {Math.Round(Math.PI * Math.Pow(radius,2),2)}");
             Console.ReadKey();
         }
-        static void CircleArea()
+        static void guessNumber()
         {
-            int radius;
+            Random rnd = new Random();
+            int number = rnd.Next(11);
+           
+            for (int i = 0; i < 3; i++)
+            {
+                int userNumber = getValue($" Enter number 0 - 10 (attempt {i + 1}): ");
+                if (userNumber == number)
+                {
+                    Console.WriteLine(" Congratulation !!! You guess the number !!!!");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            Console.WriteLine($" You didn't guess the number \"{number}\"");
+            Console.ReadKey();
+        }
+
+        static void drawRectangle()
+        {
+            int width = getValue(" Enter width of rectangle : ");
+            int height = getValue(" Enter width of height : ");
+            int X = getValue(" Enter X cootdinate : ");
+            int Y = getValue(" Enter Y cootdinate : ");
+            Console.Clear();
+            for (int y = 0; y < height; y++)
+            {
+                Console.SetCursorPosition(X, Y+y);
+                for (int i = 0; i < width; i++)
+                {
+                    if (y == 0) Console.ForegroundColor = ConsoleColor.Red;
+                    else if (y == height - 1) Console.ForegroundColor = ConsoleColor.Yellow;
+                    else if (i == 0) Console.ForegroundColor = ConsoleColor.Green;
+                    else if (i == width - 1) Console.ForegroundColor = ConsoleColor.Blue;
+                    else
+                    {
+                        Console.Write(" ");
+                        continue;
+                    }
+                   Console.Write("*");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static void harvestingHay()
+        {
+            Console.Clear();
+            int hayMowers = getValue(" Enter hay mowers count : ");
+            int  time = getValue(" Enter first hay mowers work time (hours): ") * 60;
+            for (int i = 0; i < hayMowers - 1; i++)
+                time += time + 10;
+            Console.WriteLine($" The entire team worked for {time / 60} hours and {time % 60} minutes");
+            Console.ReadKey();
+        }
+
+
+        static int  getValue(string message)
+        {
+            int value;
             do
             {
                 Console.Clear();
-                Console.Write(" Enter circle radius : ");
-            } while (!int.TryParse(Console.ReadLine(), out radius));
-            Console.Write($" Area of circle with radius {radius}  =  {Math.Round(Math.PI * Math.Pow(radius, 2), 2)}");
-            Console.ReadKey();
+                Console.Write(message);
+            } while (!int.TryParse(Console.ReadLine(), out value));
+            return value;
         }
 
     }
